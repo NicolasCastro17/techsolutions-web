4c0ce4acd63b981ac6659a46b26f05dbf6c7847e
Nicolas <nicoa.castro@duocuc.cl>
2025-10-17 00:20:09 -0300

    (RF3 EDAD) Dev2: agrega campo de edad

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const users = [
+    // Dev2 incluye 'age' y omite 'email', lo que generará el conflicto.
+    { id: 1, name: "Alice", age: 30, product: "DataViz" },
+    { id: 2, name: "Bob", age: 25, product: "TechSolutions" },
+    { id: 3, name: "Charlie", age: 40, product: "DataViz" }
+];
